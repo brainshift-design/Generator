10a3d9caf6b7a6cbd4120d1d1efcde55243de30c
Alex Bourt <alex@bourt.com>
2020-10-26 23:30:52 +0100

    plugin created

--- code.js
+++ code.js
@@ -5,6 +5,7 @@
 // full browser environment (see documentation).
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__);
+figma.ui.resize(240, 400);
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
@@ -25,5 +26,5 @@ figma.ui.onmessage = msg => {
     }
     // Make sure to close the plugin when you're done. Otherwise the plugin will
     // keep running, which shows the cancel button at the bottom of the screen.
-    figma.closePlugin();
+    //figma.closePlugin();
 };
--- code.ts
+++ code.ts
@@ -7,6 +7,7 @@
 
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__);
+figma.ui.resize(240, 400);
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
@@ -29,5 +30,5 @@ figma.ui.onmessage = msg => {
 
   // Make sure to close the plugin when you're done. Otherwise the plugin will
   // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
+  //figma.closePlugin();
 };
