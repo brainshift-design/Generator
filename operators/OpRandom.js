60d21f4ec5a070e20087eda1811b9bb5d73e29a4
Alex Bourt <alex@bourt.com>
2020-11-11 11:17:15 +0100

    limited OpRandom min/max

--- operators/OpRandom.js
+++ operators/OpRandom.js
@@ -15,6 +15,18 @@ extends Operator
         this.addParam(this.#min   = new NumberParam('min'));
         this.addParam(this.#max   = new NumberParam('max'));
         this.addParam(this.#scale = new NumberParam('scale', 1, 1));
+
+        this.#min.control.addEventListener('onchange', () =>
+        {
+            if (this.#min.control.value > this.#max.control.value)
+                this.#max.control.setValue(this.#min.control.value, false);
+        });
+
+        this.#max.control.addEventListener('onchange', () =>
+        {
+            if (this.#max.control.value < this.#min.control.value)
+                this.#min.control.setValue(this.#max.control.value, false);
+        });
     }
 
 
