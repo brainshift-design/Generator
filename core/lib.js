3388da4df4efd5a396bdd5a7ef01512c165e5311
Alex Bourt <alex@bourt.com>
2021-10-23 17:04:40 +0200

    keygen crypto works

--- core/lib.js
+++ core/lib.js
@@ -151,3 +151,21 @@ function arrayToString(bytes)
 
     return str;
 }
+
+
+
+function getQueryVariable(strVar)
+{
+    var query = window.location.search.substring(1);
+    var vars  = query.split('&');
+
+    for (var i = 0; i < vars.length; i++) 
+    {
+        var pair = vars[i].split('=');
+
+        if (pair[0] == strVar)
+            return pair[1];
+    }
+
+    return false;
+}
