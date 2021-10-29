875befc74088dc822106b4ddd8c0c8f29e613fb1
Alex Bourt <alex@bourt.com>
2021-10-29 14:03:45 +0200

    added basic load

--- storage/storage.js
+++ storage/storage.js
@@ -1,12 +1,51 @@
 function saveToLocalFile(filename, str) 
 {
-    var link = document.createElement('a');
+    const link = document.createElement('a');
     link.style.display = 'none';
-
+    
     link.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(str));
     link.setAttribute('download', filename);
-      
+    
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
+}
+
+
+
+function loadFromLocalFile()
+{
+    const input = document.createElement('input');
+
+    input.type          = 'file';
+    input.style.display = 'none';
+
+    input.addEventListener('change', e =>
+    {
+        const files = e.target.files;
+
+        if (files.length > 0)
+            loadGraphFromFile(files[0]);
+    },
+    false);
+
+    //document.body.appendChild(input);
+    input.click();
+    //document.body.removeChild(input);
+}
+
+
+
+function loadGraphFromFile(file)
+{
+    const reader = new FileReader();
+
+    reader.onload = () => 
+    {
+        console.log(reader.result);
+        // clear uiGraph
+        // load file contents
+    };
+
+    reader.readAsText(file);
 }
--- ui/ui events.js
+++ ui/ui events.js
@@ -2,28 +2,7 @@ menuSelect.addEventListener('change', async function(e)
 {
     switch (e.detail.selectedValue)
     {
-        case 'saveLocal':
-        {
-            let save = uiGraph.save();
-
-            // const options = 
-            // {
-            //     types: 
-            //     [{
-            //         description: 'Text Files',
-            //         accept:      { 'text/plain': ['.txt'] }
-            //     }],
-            // };
-
-            // const handle = await window.showSaveFilePicker(options);
-
-            //setPluginData('generator', save);
-            
-            saveToLocalFile('graph.gen', save);
-            break;
-        }
-
-        case 'load':
-            break;
+        case 'saveLocal': saveToLocalFile('graph.gen', uiGraph.save()); break;
+        case 'loadLocal': loadFromLocalFile();                          break;
     }
 });
