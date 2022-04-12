56ce892e63da265a2c853c3472f1449e414c09ed
Alex Bourt <alex@bourt.com>
2022-04-12 16:25:33 +0200

    disconnect actions work with active

--- actions/DisconnectAction.js
+++ actions/DisconnectAction.js
@@ -9,6 +9,9 @@ extends Action
     inputIndex;
     get inputOp() { return nodeFromId(this.inputOpId); }
 
+    newActiveOpIds    = [];
+    oldActiveOpIds = [];
+
 
 
     constructor(output, input)
@@ -28,6 +31,9 @@ extends Action
 
         this.inputOpId   = input.op.id;
         this.inputIndex  = inputIndex;
+
+
+        this.oldActiveOpIds = [...getActiveNodesInTreeFrom(nodeFromId(this.inputOpId)).map(n => n.id)];
     }
 
 
@@ -35,9 +41,22 @@ extends Action
     do()
     {
         uiDisconnect(this.inputOp.inputs[this.inputIndex]);
+        
+        if (!getActiveNodeInTreeFrom(this.inputOp))
+        {
+            uiMakeNodeActive(this.inputOp);
+            this.newActiveOpIds.push(this.inputOpId);
+            this.inputOp.pushUpdate();
+            //graphView.updateNodeTransform(this.inputOp);
+        }
 
-        this.inputOp.pushUpdate();
-        //graphView.updateNodeTransform(this.inputOp);
+        if (!getActiveNodeInTreeFrom(this.outputOp))
+        {
+            uiMakeNodeActive(this.outputOp);
+            this.newActiveOpIds.push(this.outputOpId);
+            this.outputOp.pushUpdate();
+            //graphView.updateNodeTransform(this.outputOp);
+        }
     }
     
     
@@ -45,12 +64,40 @@ extends Action
     undo()
     {
         uiVariableConnect(
-            this.outputOp, 
-            this.outputIndex, 
-            this.inputOp, 
-            this.inputIndex);
+            this.outputOp, this.outputIndex, 
+            this. inputOp, this. inputIndex);
+
+        for (const id of this.newActiveOpIds)
+            uiMakeNodePassive(nodeFromId(id));
+
+        let oldActiveOpIds = [...this.oldActiveOpIds];
+        oldActiveOpIds.sort((x, y) => (nodeFromId(x) === nodeFromId(y)) ? 0 : nodeFromId(y).follows(nodeFromId(x)) ? -1 : 1);
+
+        for (const id of oldActiveOpIds)
+            uiMakeNodeActive(nodeFromId(id));
 
         //graphView.updateNodeTransform(this.inputOp);
         this.inputOp.pushUpdate();
     }
+    
+    
+    
+    redo()
+    {
+        uiDisconnect(this.inputOp.inputs[this.inputIndex]);
+        
+        if (!getActiveNodeInTreeFrom(this.inputOp))
+        {
+            uiMakeNodeActive(this.inputOp);
+            this.inputOp.pushUpdate();
+            //graphView.updateNodeTransform(this.inputOp);
+        }
+
+        if (!getActiveNodeInTreeFrom(this.outputOp))
+        {
+            uiMakeNodeActive(this.outputOp);
+            this.outputOp.pushUpdate();
+            //graphView.updateNodeTransform(this.outputOp);
+        }
+   }
 }
