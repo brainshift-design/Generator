83025d026919f3bbc6e78fc82042c5b0d4140233
Alex Bourt <alex@bourt.com>
2023-04-03 01:49:05 +0200

    fixed fit to rect

--- controls/graphView/graphView pan & zoom.js
+++ controls/graphView/graphView pan & zoom.js
@@ -211,8 +211,8 @@ GraphView.prototype.zoomToRect = function(rect, margin = 40)
 
     this.zoom = 
         viewRect.width >= viewRect.height
-        ? (viewRect.height - margin*2) / rect.height
-        : (viewRect.width  - margin*2) / rect.width;
+        ? (rect.width >= rect.height ? (viewRect.width  - margin*2) / rect.width  : (viewRect.height - margin*2) / rect.height)
+        : (rect.width <  rect.height ? (viewRect.height - margin*2) / rect.height : (viewRect.width  - margin*2) / rect.width );
 
     this.pan = {
         x: viewRect.width /2 - (rect.x + rect.width /2) * this.zoom,
