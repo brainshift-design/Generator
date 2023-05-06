add912324654ed5e639e9bf2c9df4352572c645c
Alex Bourt <alex@bourt.com>
2023-05-06 11:48:21 +0200

    Update random.js

--- core/random.js
+++ core/random.js
@@ -1,14 +1,18 @@
 class Random
 {
     seed;
-    last;
+    index; // for next()
+
+    cache;
 
 
 
-    constructor(seed = 0, last = seed)//(seed = 0xb9ef7ca4, last = seed)
+    constructor(seed = 0)
     { 
-        this.seed = seed; 
-        this.last = last;
+        this.seed  = seed; 
+        this.index = 0;
+        
+        this.updateCache(256);
     }
 
 
@@ -20,18 +24,50 @@ class Random
 
 
 
-    next()
+    updateCache(size)
     {
-        this.last = this.seed;
-        
-        this.seed = (this.seed + 0x7ed55d16) + (this.seed << 12);
-        this.seed = (this.seed ^ 0xc761c23c) ^ (this.seed >> 19);
-        this.seed = (this.seed + 0x165667b1) + (this.seed <<  5);
-        this.seed = (this.seed + 0xd3a2646c) ^ (this.seed <<  9);
-        this.seed = (this.seed + 0xfd7046c5) + (this.seed <<  3);
-        this.seed = (this.seed ^ 0xb55a4f09) ^ (this.seed >> 16);
+        this.cache = new Int32Array(size);
 
-        return this.seed / -0x7fffffff;
+        let seed = this.seed;
+        let last = seed;
+
+        for (let i = 0; i < size; i++)
+            this.cache[i] = seed = this.generate(seed, last);
     }
 
+
+
+    next()
+    {
+        if (this.index >= this.cache.length)
+            this.updateCache(nextPow2(this.index));
+
+        return this.cache[this.index++] / -0x7fffffff;
+    }
+
+
+
+    get(index)
+    {
+        if (index >= this.cache.length)
+            this.updateCache(nextPow2(index));
+
+        return this.cache[index] / -0x7fffffff;
+    }
+
+
+
+    generate(seed, last)
+    {
+        last = seed;
+        
+        seed = (seed + 0x7ed55d16) + (seed << 12);
+        seed = (seed ^ 0xc761c23c) ^ (seed >> 19);
+        seed = (seed + 0x165667b1) + (seed <<  5);
+        seed = (seed + 0xd3a2646c) ^ (seed <<  9);
+        seed = (seed + 0xfd7046c5) + (seed <<  3);
+        seed = (seed ^ 0xb55a4f09) ^ (seed >> 16);
+
+        return seed;
+    }
 }
