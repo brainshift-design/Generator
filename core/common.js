50ef5eb8d0a2364a62ae9e015debc52dbb37917d
Brainshift <brainshift.design@gmail.com>
2023-06-29 00:05:51 +0200

    fixed colorparam bug

--- core/common.js
+++ core/common.js
@@ -1,4 +1,4 @@
-const generatorVersion = 139;
+const generatorVersion = 140;
 
 
 const MAX_INT32        = 2147483647;
--- output/main.ts
+++ output/main.ts
@@ -23,7 +23,7 @@ function noNodeTag(key) { return noTag(key, nodeTag); }
 function noConnTag(key) { return noTag(key, connTag); }
 
 
-const generatorVersion = 139;
+const generatorVersion = 140;
 
 
 const MAX_INT32        = 2147483647;
