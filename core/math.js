67eeecda2e2d57edb2e898166c5de97f8aff8f08
Brainshift <136026582+brainshift-design@users.noreply.github.com>
2024-03-16 07:28:02 +0100

    finished interpolate

--- core/math.js
+++ core/math.js
@@ -385,6 +385,15 @@ function uintToBufferAt(val, buffer, start, bufferSize)
 
 
 
+function lerpCos(a, b, t)
+{
+    return a < b
+         ? a + (b - a) * (-Math.cos(t*Tau/2)+1)/2
+         : a + (b - a) * (-Math.cos(t*Tau/2)+1)/2;
+}
+
+
+
 function lerp(a, b, t)
 {
     return a + (b - a) * t;
