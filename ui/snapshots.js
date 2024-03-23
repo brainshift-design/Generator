56de56d2398710a8c1abeb5cda253a0e79d1c426
Brainshift <136026582+brainshift-design@users.noreply.github.com>
2024-03-23 22:24:51 +0100

    figured out default snapshot thumbnail size

--- ui/snapshots.js
+++ ui/snapshots.js
@@ -1,6 +1,6 @@
 function initSnapshots()
 {
-    for (let i = 0; i < 10; i++)
+    for (let i = 0; i < 20; i++)
     {
         const thumb = createDiv('snapshotThumbnail');
         snapshotBar.appendChild(thumb);
