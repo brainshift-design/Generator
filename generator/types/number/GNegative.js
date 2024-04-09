c1188e4fb8bf458132e9e474c4059447be9775e2
Brainshift <136026582+brainshift-design@users.noreply.github.com>
2024-04-09 17:50:37 +0200

    OpNegative accepts lists

--- generator/types/number/GNegative.js
+++ generator/types/number/GNegative.js
@@ -30,24 +30,48 @@ extends GOperator1
             
         if (input)
         {
-            this.value = input;
+            if (isListValueType(input.type))
+            {
+                this.value = new ListValue();
 
-            consoleAssert(
-                this.value.type == NUMBER_VALUE, 
-                'this.value.type must be NUMBER_VALUE');
+                for (let i = 0; i < input.items.length; i++)
+                {
+                    const item = input.items[i];
 
-            if (this.options.enabled)
-                this.value.value = -this.value.value;
+                    this.value.items.push(
+                        item.type == NUMBER_VALUE
+                        ? getNegativeValue(item, this.options.enabled)
+                        : NumberValue.NaN.copy());   
+                }
+            }
+            else
+                this.value = getNegativeValue(input, this.options.enabled);
         }
         else
             this.value = NumberValue.NaN.copy();
 
 
-        this.setUpdateValues(parse, [['value', this.value]]);
+        this.setUpdateValues(parse, 
+        [
+            ['type', this.outputType()]
+        ]);
 
 
         this.validate();
 
         return this;
     }
+}
+
+
+
+function getNegativeValue(input, enabled)
+{
+    consoleAssert(
+         input == NUMBER_VALUE, 
+        'input must be NUMBER_VALUE');
+
+    return new NumberValue(
+        (enabled ? -1 : 1) * input.value,
+        input.decimals);
 }
--- operators/number/OpNegative.js
+++ operators/number/OpNegative.js
@@ -10,7 +10,7 @@ extends OperatorBase
         this.iconOffsetY = -1;
         
 
-        this.addInput (new Input (NUMBER_TYPES));
+        this.addInput (new Input ([NUMBER_VALUE, NUMBER_LIST_VALUE, LIST_VALUE]));
         this.addOutput(new Output([NUMBER_VALUE], this.output_genRequest));
     }
 
@@ -41,6 +41,37 @@ extends OperatorBase
         pushUnique(gen.passedNodes, this.node);
 
         return request;
+    }
+
+
+
+    updateValues(requestId, actionId, updateParamId, paramIds, values)
+    {
+        const type = values[paramIds.findIndex(id => id == 'type')];
+
+        if (type)
+            this.headerOutputs[0].types = [type.value];
+
+        super.updateValues(requestId, actionId, updateParamId, paramIds, values);
+    }
+
+
+
+    getHeaderColors(options = {})
+    {
+        const colors = super.getHeaderColors(options);
+        const type   = this.outputs[0].types[0];
+
+        colors.text  = isDark(colors.back) ? [1, 1, 1, 1] : [0, 0, 0, 1]; 
+
+        const gray =
+               this.active
+            && this.outputs[0].types[0] == LIST_VALUE;
+
+        colors.output  = gray ? rgb_a(colors.text, 0.35) : rgb_a(rgbSaturateHsv(rgbFromType(type, true), 0.5), 0.7);
+        colors.outWire = rgbFromType(type, true);
+
+        return colors;
     }
 
 
