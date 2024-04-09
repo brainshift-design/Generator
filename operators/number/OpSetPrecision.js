dea354ffcaf48157a9230079371b0dc31065956a
Brainshift <136026582+brainshift-design@users.noreply.github.com>
2024-04-09 17:06:57 +0200

    set precision supports lists

--- operators/number/OpSetPrecision.js
+++ operators/number/OpSetPrecision.js
@@ -13,7 +13,7 @@ extends OperatorBase
         //this.iconOffsetY = -2;
         
 
-        this.addInput (new Input (NUMBER_TYPES));
+        this.addInput (new Input ([NUMBER_VALUE, NUMBER_LIST_VALUE, LIST_VALUE]));
         this.addOutput(new Output([NUMBER_VALUE], this.output_genRequest));
 
         this.addParam(this.paramDecimals = new NumberParam('decimals', 'decimals', false, true,  true, 0, 0, 10));
@@ -53,10 +53,41 @@ extends OperatorBase
 
 
 
+    updateValues(requestId, actionId, updateParamId, paramIds, values)
+    {
+        const type = values[paramIds.findIndex(id => id == 'type')];
+
+        if (type)
+            this.headerOutputs[0].types = [type.value];
+
+        super.updateValues(requestId, actionId, updateParamId, paramIds, values);
+    }
+
+
+
     updateParams()
     {
         this.paramDecimals.enableControlText(true, this.paramDecimals.isUnknown());
 
         this.updateParamControls();
+    }
+
+
+
+    getHeaderColors(options = {})
+    {
+        const colors = super.getHeaderColors(options);
+        const type   = this.outputs[0].types[0];
+
+        colors.text  = isDark(colors.back) ? [1, 1, 1, 1] : [0, 0, 0, 1]; 
+
+        const gray =
+               this.active
+            && this.outputs[0].types[0] == LIST_VALUE;
+
+        colors.output  = gray ? rgb_a(colors.text, 0.35) : rgb_a(rgbSaturateHsv(rgbFromType(type, true), 0.5), 0.7);
+        colors.outWire = rgbFromType(type, true);
+
+        return colors;
     }
 }
