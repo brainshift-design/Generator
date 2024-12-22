977ae5d503d40d0c2d602b20a3253b0b037aebb1
shamsi <brainshift.design@gmail.com>
2024-12-22 14:26:06 +0100

    fixed more bugs

--- generator/types/number/GBoolean.js
+++ generator/types/number/GBoolean.js
@@ -153,3 +153,296 @@ extends GArithmetic
         return bool;
     }
 }
+
+
+
+async function evalAndInputs(inputs, parse)
+{
+    if (isEmpty(inputs))
+        return NumberValue.NaN();
+
+
+    const value = new NumberValue();
+
+
+    if (!isEmpty(inputs))
+    {
+        const val0 = await evalNumberOrListValue(inputs[0], parse);
+        if (!val0.isValid()) return NumberValue.NaN();
+
+
+        if (    isListValueType(val0.type)
+            && !isEmpty(val0.items))
+        {
+            const item0 = val0.items[0];
+
+            value.value = item0.toNumber();
+
+            for (let i = 1; i < val0.items.length; i++)
+            {
+                const item = val0.items[i];
+                
+                if (item.type == NUMBER_VALUE)
+                    value.value = Math.min(value.value, item.toNumber());
+            }
+        }
+        else
+        {
+            if (val0.type != NUMBER_VALUE)
+                return NumberValue.NaN();
+
+            value.value = val0.toNumber();
+        }
+
+
+        for (let i = 1; i < inputs.length; i++)
+        {
+            const val = await evalNumberOrListValue(inputs[i], parse);
+            if (!val.isValid()) return NumberValue.NaN();
+
+
+            if (isListValueType(val.type))
+            {
+                for (const item of val.items)
+                {
+                    if (item.type == NUMBER_VALUE)
+                        value.value = Math.min(value.value, item.toNumber());
+                }
+            }
+            else
+            {
+                consoleAssert(
+                    val.type == NUMBER_VALUE, 
+                    'val.type must be NUMBER_VALUE');
+
+                value.value = Math.min(value.value, val.toNumber());
+            }
+        }
+
+
+        if (value.value != 0)
+            value.value = 1;
+    }
+
+
+    return value;
+}
+
+
+
+async function evalNandInputs(inputs, parse)
+{
+    if (isEmpty(inputs))
+        return NumberValue.NaN();
+
+
+    const value = new NumberValue();
+
+
+    if (!isEmpty(inputs))
+    {
+        const val0 = await evalNumberOrListValue(inputs[0], parse);
+        if (!val0.isValid()) return NumberValue.NaN();
+
+        if (    isListValueType(val0.type)
+            && !isEmpty(val0.items))
+        {
+            const item0 = val0.items[0];
+
+            value.value = item0.toNumber() != 0 ? 0 : 1;
+
+            for (let i = 1; i < val0.items.length; i++)
+            {
+                const item = val0.items[i];
+                
+                if (   item.type == NUMBER_VALUE
+                    && item.toNumber() == 0)
+                    value.value = 1;
+            }
+        }
+        else
+        {
+            if (val0.type != NUMBER_VALUE)
+                return NumberValue.NaN();
+
+            value.value = val0.toNumber() != 0 ? 0 : 1;
+        }
+
+
+        for (let i = 1; i < inputs.length; i++)
+        {
+            const val = await evalNumberOrListValue(inputs[i], parse);
+            if (!val.isValid()) return NumberValue.NaN();
+
+            if (isListValueType(val.type))
+            {
+                for (const item of val.items)
+                {
+                    if (   item.type == NUMBER_VALUE
+                        && item.toNumber() == 0)
+                        value.value = 1;
+                }
+            }
+            else
+            {
+                consoleAssert(
+                    val.type == NUMBER_VALUE, 
+                    'val.type must be NUMBER_VALUE');
+
+                if (val.toNumber() == 0)
+                    value.value = 1;
+            }
+        }
+    }
+
+
+    return value;
+}
+
+
+
+async function evalOrInputs(inputs, parse)
+{
+    if (isEmpty(inputs))
+        return NumberValue.NaN();
+
+
+    const value = new NumberValue();
+
+
+    if (!isEmpty(inputs))
+    {
+        const val0 = await evalNumberOrListValue(inputs[0], parse);
+        if (!val0.isValid()) return NumberValue.NaN();
+
+        if (    isListValueType(val0.type)
+            && !isEmpty(val0.items))
+        {
+            const item0 = val0.items[0];
+
+            value.value = item0.toNumber();
+
+            for (let i = 1; i < val0.items.length; i++)
+            {
+                const item = val0.items[i];
+                
+                if (item.type == NUMBER_VALUE)
+                    value.value = Math.max(value.value, item.toNumber());
+            }
+        }
+        else
+        {
+            if (val0.type != NUMBER_VALUE)
+                return NumberValue.NaN();
+
+            value.value = val0.toNumber();
+        }
+
+
+        for (let i = 1; i < inputs.length; i++)
+        {
+            const val = await evalNumberOrListValue(inputs[i], parse);
+            if (!val.isValid()) return NumberValue.NaN();
+
+            if (isListValueType(val.type))
+            {
+                for (const item of val.items)
+                {
+                    if (item.type == NUMBER_VALUE)
+                        value.value = Math.max(value.value, item.toNumber());
+                }
+            }
+            else
+            {
+                consoleAssert(
+                    val.type == NUMBER_VALUE, 
+                    'val.type must be NUMBER_VALUE');
+                    
+                value.value = Math.max(value.value, val.toNumber());
+            }
+        }
+
+
+        if (value.value != 0)
+            value.value = 1;
+    }
+
+
+    return value;
+}
+
+
+
+async function evalXorInputs(inputs, parse)
+{
+    if (isEmpty(inputs))
+        return NumberValue.NaN();
+
+
+    const value = new NumberValue(0);
+
+
+    let flipped;
+
+    if (!isEmpty(inputs))
+    {
+        const val0 = await evalNumberOrListValue(inputs[0], parse);
+        if (!val0.isValid()) return NumberValue.NaN();
+
+        if (    isListValueType(val0.type)
+            && !isEmpty(val0.items))
+        {
+            const item0 = val0.items[0];
+
+            flipped = item0.toNumber() != 0;
+
+            for (let i = 1; i < val0.items.length; i++)
+            {
+                const item = val0.items[i];
+                
+                if (   item.type == NUMBER_VALUE
+                    && item.toNumber() != 0)
+                    flipped++;
+            }
+        }
+        else
+        {
+            if (val0.type != NUMBER_VALUE)
+                return NumberValue.NaN();
+
+            flipped = val0.toNumber() != 0;
+        }
+
+
+        for (let i = 1; i < inputs.length; i++)
+        {
+            const val = await evalNumberOrListValue(inputs[i], parse);
+            if (!val.isValid()) return NumberValue.NaN();
+
+            if (isListValueType(val.type))
+            {
+                for (const item of val.items)
+                {
+                    if (   item.type == NUMBER_VALUE
+                        && item.toNumber() != 0)
+                        flipped++;
+                    }
+            }
+            else
+            {
+                consoleAssert(
+                    val.type == NUMBER_VALUE, 
+                    'val.type must be NUMBER_VALUE');
+                    
+                if (val.toNumber() != 0)
+                    flipped++;
+            }
+        }
+
+
+        value.value = flipped == 1 ? 1 : 0;
+    }
+
+
+    return value;
+}
